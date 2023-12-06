260b08777b9cb999b1217ee52bd038686ad39c2d
angelsebamc <asmcardozoo@gmail.com>
2023-12-06 11:34:40 -0300

    working on single player logic

--- lib/tic_tac_dart.dart
+++ lib/tic_tac_dart.dart
@@ -8,19 +8,20 @@ class TicTacDart {
   List<String> playerTwo = [];
 
   List<List<String>> winningPositions = [
-    ['0 0', '0 1', '0 2'],
-    ['1 0', '1 1', '1 2'],
-    ['2 0', '2 1', '2 2'],
-    ['0 0', '1 0', '2 0'],
-    ['1 1', '2 1', '2 1'],
-    ['2 0', '2 1', '2 2'],
-    ['0 0', '1 1', '2 2'],
-    ['0 2', '1 1', '2 0'],
+    ['1 1', '1 2', '1 3'],
+    ['2 1', '2 2', '2 3'],
+    ['3 1', '3 2', '3 3'],
+    ['1 1', '2 1', '3 1'],
+    ['1 2', '2 2', '3 2'],
+    ['1 3', '2 3', '3 3'],
+    ['1 1', '2 2', '3 3'],
+    ['1 3', '2 2', '3 1'],
   ];
 
   bool playerOneTurn = false;
+  bool completed = false;
 
-  int totalTurns = 9;
+  int totalTurns = 4;
   List<List<String>> table = [];
   Map<String, String> positions = {
     '1 1': '0 0',
@@ -58,27 +59,47 @@ class TicTacDart {
   }
 
   void singlePlayer() {
-    while (totalTurns > 0) {
-      if (playerOneTurn) {
-        stdout.writeln("Select a position: ");
-        playerPosition = stdin.readLineSync();
+    printTable();
+    while (totalTurns > 0 && !completed) {
+      //if (playerOneTurn) {
+      stdout.writeln("Select a position: ");
+      playerPosition = stdin.readLineSync();
 
-        String? findPosition = positions[playerPosition];
+      totalTurns--;
 
-        List<String> selectedPosition = findPosition!.split(' ');
+      String? findPosition = positions[playerPosition];
 
-        int rowPosition = selectedPosition[0] as int;
-        int columnPosition = selectedPosition[1] as int;
+      List<String> selectedPosition = findPosition!.split(' ');
 
-        String tablePosition = table[rowPosition][columnPosition];
-        
-        if(['x', 'o'].contains(tablePosition)) {
-          stdout.writeln("The position is already used. Selected another one");
-          return;
+      int rowPosition = int.parse(selectedPosition[0]);
+      int columnPosition = int.parse(selectedPosition[1]);
+
+      String tablePosition = table[rowPosition][columnPosition];
+
+      if (['x', 'o'].contains(tablePosition)) {
+        stdout.writeln("The position is already used. Selected another one");
+        return;
+      }
+
+      table[rowPosition][columnPosition] = 'x';
+      playerOne.add(playerPosition!);
+      printTable();
+
+      if (playerOne.length == 3) {
+        for (var element in winningPositions) {
+          if (element[0] == playerOne[0] &&
+              element[1] == playerOne[1] &&
+              element[2] == playerOne[2]) {
+            completed = true;
+          }
         }
 
-
+        if (completed) {
+          stdout.writeln("Player 1 has won");
+          return;
+        }
       }
+      //}
     }
   }
 }
